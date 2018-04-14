5075149f4e688b83e89160cbda462b153f698538
Nick Saponaro <nicksapsales@gmail.com>
2018-04-13 17:39:21 -0700

    divify and update deps

--- config.js
+++ config.js
@@ -41,13 +41,13 @@ var config = {
     btc: {
       livenet: {
         provider: 'insight',
-        url: 'https://insight.dashevo.org:443',
-        apiPrefix:'/insight-api-dash'
+        url: 'https://insight.diviproject.org:443',
+        apiPrefix:'/insight-api-divi'
       },
       testnet: {
         provider: 'insight',
-        url: 'https://testnet-insight.dashevo.org:443',
-        apiPrefix:'/insight-api-dash'
+        url: 'https://testnet-insight.diviproject.org:443',
+        apiPrefix:'/insight-api-divi'
         // url: 'http://localhost:3001',
         // Multiple servers (in priority order)
         // url: ['http://a.b.c', 'https://test-insight.bitpay.com:443'],
